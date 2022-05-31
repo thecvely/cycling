cc4980e8705c5460f243c9b1f513e6e8cbe06fae
thecvely <thecvely@gmail.com>
2022-05-30 23:54:25 -0500

    Envio de variables de sesion a html

--- rutas/root.js
+++ rutas/root.js
@@ -1,20 +1,45 @@
 const express=require('express')
 const router=express.Router()
 const {pool}=require('../modelos/database/mqtt_user.js')
+
+
+
 router.get('/', (req, res)=>{
 
 
+    const sesion={
+        enable:'',
+        name:'',
+        lastname:'',
+        nav_h:{
+            b1:'',
+            b2:''
+        }
+    }
+    
+    const error={
+        class:'',
+        data:''
+    }
+
     if (req.session.login===true)
     {
+        sesion.enable=true
+        sesion.name=req.session.username
+        sesion.lastname=req.session.lastname
+        sesion.nav_h.b1="display:block"
+        sesion.nav_h.b2="Añadir"
+
         console.log('Sesion Iniciada')
-        res.render('usuarios/inicio.ejs')
+        res.render('usuarios/inicio.ejs', {error, sesion})
     } else {
-        const error={
-            class:'',
-            data:''
-        }
+        sesion.enable=false
+        sesion.name='Right'
+        sesion.lastname='Cycling'
+        sesion.nav_h.b1="display:none"
+        sesion.nav_h.b2="Registrar"
         console.log('Iniciar Session')
-        res.render('usuarios/login.ejs', {error})
+        res.render('usuarios/login.ejs', {error, sesion})
 
     }
 
